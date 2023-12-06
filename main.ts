c78a4dd2901695460ba4eb87c4b33974e9886186
william <124389603+william-simard@users.noreply.github.com>
2023-12-06 13:04:26 -0500

    save v2

--- main.ts
+++ main.ts
@@ -2,19 +2,28 @@
  *
  * Created by: william simard
  * Created on: dec 2023
- * This program ...
+ * This program bluetooth radio stuff
 */
 
 // variable
 let distance: Number = 0
 
 //setup
-radio.setGroup(87)
+radio.setGroup(7)
+basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
-input.onButtonPressed(Button.A, function () { 
-    distance = 
-    DigitalPin.P0,
-    DigitalPin.P1,
-    PingUnit.Centimeters
-   })
+input.onButtonPressed(Button.A, function () {
+    distance = sonar.ping( 
+        DigitalPin.P0,
+        DigitalPin.P1,
+        PingUnit.Centimeters
+  )
+{ 
+
+radio.onReceivedString(function (receivedString) {
+    basic.clearScreen()
+    basic.showString(receivedString)
+    basic.showIcon(IconNames.Happy)
+ })
+}
